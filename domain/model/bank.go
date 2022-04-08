1e9a474eb69ab0b1e6d25c84101a379cc99ac844
netoudi <netoudi@rocketmail.com>
2022-04-07 23:23:23 -0300

    style: change indent style from space to tab

--- domain/model/bank.go
+++ domain/model/bank.go
@@ -1,39 +1,39 @@
 package model
 
 import (
-    "github.com/asaskevich/govalidator"
-    uuid "github.com/satori/go.uuid"
-    "time"
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+	"time"
 )
 
 type Bank struct {
-    Base     `valid:"required"`
-    Code     string     `json:"code" gorm:"type:varchar(20)" valid:"notnull"`
-    Name     string     `json:"name" gorm:"type:varchar(255)" valid:"notnull"`
-    Accounts []*Account `gorm:"ForeignKey:BankID" valid:"-"`
+	Base     `valid:"required"`
+	Code     string     `json:"code" gorm:"type:varchar(20)" valid:"notnull"`
+	Name     string     `json:"name" gorm:"type:varchar(255)" valid:"notnull"`
+	Accounts []*Account `gorm:"ForeignKey:BankID" valid:"-"`
 }
 
 func (bank *Bank) isValid() error {
-    _, err := govalidator.ValidateStruct(bank)
-    if err != nil {
-        return err
-    }
-    return nil
+	_, err := govalidator.ValidateStruct(bank)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func NewBank(code string, name string) (*Bank, error) {
-    bank := Bank{
-        Code: code,
-        Name: name,
-    }
+	bank := Bank{
+		Code: code,
+		Name: name,
+	}
 
-    bank.ID = uuid.NewV4().String()
-    bank.CreatedAt = time.Now()
+	bank.ID = uuid.NewV4().String()
+	bank.CreatedAt = time.Now()
 
-    err := bank.isValid()
-    if err != nil {
-        return nil, err
-    }
+	err := bank.isValid()
+	if err != nil {
+		return nil, err
+	}
 
-    return &bank, nil
+	return &bank, nil
 }
--- infrastructure/db/db.go
+++ infrastructure/db/db.go
@@ -1,54 +1,54 @@
 package db
 
 import (
-    "github.com/netoudi/codepix-central/domain/model"
-    "log"
-    "os"
-    "path/filepath"
-    "runtime"
+	"github.com/netoudi/codepix-central/domain/model"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
 
-    "github.com/jinzhu/gorm"
-    "github.com/joho/godotenv"
-    _ "github.com/lib/pq"
-    _ "gorm.io/driver/sqlite"
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	_ "gorm.io/driver/sqlite"
 )
 
 func init() {
-    _, b, _, _ := runtime.Caller(0)
-    basepath := filepath.Dir(b)
+	_, b, _, _ := runtime.Caller(0)
+	basepath := filepath.Dir(b)
 
-    err := godotenv.Load(basepath + "/../../.env")
+	err := godotenv.Load(basepath + "/../../.env")
 
-    if err != nil {
-        log.Fatalf("Error loading .env files")
-    }
+	if err != nil {
+		log.Fatalf("Error loading .env files")
+	}
 }
 
 func ConnectDB(env string) *gorm.DB {
-    var dsn string
-    var db *gorm.DB
-    var err error
+	var dsn string
+	var db *gorm.DB
+	var err error
 
-    if env != "test" {
-        dsn = os.Getenv("DSN")
-        db, err = gorm.Open(os.Getenv("DB_TYPE"), dsn)
-    } else {
-        dsn = os.Getenv("DSN_TEST")
-        db, err = gorm.Open(os.Getenv("DB_TYPE_TEST"), dsn)
-    }
+	if env != "test" {
+		dsn = os.Getenv("DSN")
+		db, err = gorm.Open(os.Getenv("DB_TYPE"), dsn)
+	} else {
+		dsn = os.Getenv("DSN_TEST")
+		db, err = gorm.Open(os.Getenv("DB_TYPE_TEST"), dsn)
+	}
 
-    if err != nil {
-        log.Fatalf("Error connecting to database: %v", err)
-        panic(err)
-    }
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+		panic(err)
+	}
 
-    if os.Getenv("DEBUG") == "true" {
-        db.LogMode(true)
-    }
+	if os.Getenv("DEBUG") == "true" {
+		db.LogMode(true)
+	}
 
-    if os.Getenv("AUTO_MIGRATE_DB") == "true" {
-        db.AutoMigrate(&model.Bank{}, &model.Account{}, &model.PixKey{}, &model.Transaction{})
-    }
+	if os.Getenv("AUTO_MIGRATE_DB") == "true" {
+		db.AutoMigrate(&model.Bank{}, &model.Account{}, &model.PixKey{}, &model.Transaction{})
+	}
 
-    return db
+	return db
 }
